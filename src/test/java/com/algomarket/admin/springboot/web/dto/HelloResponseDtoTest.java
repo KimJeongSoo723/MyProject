1e8aa258cecbb65e29f80cca5149d01f7bcdbc81
KimJeongSoo723 <rlawjdtn212@gmail.com>
2022-05-08 00:07:00 +0900

    add chapter2

--- src/test/java/com/algomarket/admin/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/algomarket/admin/springboot/web/dto/HelloResponseDtoTest.java
@@ -9,14 +9,14 @@ public class HelloResponseDtoTest {
     public void 롬복_기능_테스트(){
         //given
         String name = "test";
-        int amount = 100;
+        int amount = 1000;
 
         //when
         HelloResponseDto dto = new HelloResponseDto(name,amount);
 
         //then
         assertThat(dto.getName()).isEqualTo(name);
-        assertThat(dto.getAmount()).isEqualTo(100);
+        assertThat(dto.getAmount()).isEqualTo(1000);
 
     }
 }
