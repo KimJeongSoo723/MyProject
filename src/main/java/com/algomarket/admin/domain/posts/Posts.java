17ebe00576c807b70dd3b5c7f0c5bbc065e32853
KimJeongSoo723 <rlawjdtn212@gmail.com>
2022-05-08 18:36:09 +0900

    fixed chapter3

---
+++ src/main/java/com/algomarket/admin/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.algomarket.admin.domain.posts;
+
+import jdk.jfr.events.CertificateId;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
