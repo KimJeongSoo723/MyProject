56bed42565e7986c6bc3db419266f7f3de813fca
KimJeongSoo723 <rlawjdtn212@gmail.com>
2022-05-12 16:52:53 +0900

    fixed chapter4

--- src/main/java/com/algomarket/admin/service/PostsService.java
+++ src/main/java/com/algomarket/admin/service/PostsService.java
@@ -2,13 +2,15 @@ package com.algomarket.admin.service;
 
 import com.algomarket.admin.domain.posts.Posts;
 import com.algomarket.admin.domain.posts.PostsRepository;
+import com.algomarket.admin.web.dto.PostsListResponseDto;
 import com.algomarket.admin.web.dto.PostsResponseDto;
 import com.algomarket.admin.web.dto.PostsSaveRequestDto;
 import com.algomarket.admin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.swing.text.html.parser.Entity;
 
 @RequiredArgsConstructor
@@ -26,7 +28,23 @@ public class PostsService {
     public Long update(Long id, PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("해당 게시글이 없습니다. id = "  + id));
+        posts.update(requestDto.getTitle(),requestDto.getContent());
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById (Long id){
